7a7438cd556dd00be9e5d200ec6878915a2eacc3
Ullapool <thomaslang82@bluewin.ch>
2019-01-10 21:34:52 +0100

    Added a new Code

---
+++ Zeichenkette/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+int calculate(string input){
+
+}
+
+int main()
+{
+    string input = "ABBCCC";
+    cout<<calculate(input)<<endl;
+    return 0;
+}
