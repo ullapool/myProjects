5d8ff8935f6c69808a57328a36e010269f062830
Ullapool <thomaslang82@bluewin.ch>
2019-10-14 07:50:09 +0200

    updated item list

--- smartFridge/addRemove.php
+++ smartFridge/addRemove.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="style.css"  type="text/css" media="screen" >
 </head>
     <body id="overview">
 
@@ -8,6 +9,31 @@
         <button class="button" ></button>
     </div>
 </body>
+
+<h1><b>Current items-list</b></h1>
+<table>
+    <tr>
+        <th>Product</th>
+        <th>Amount</th>
+        <th>Edit</th>
+        <th>Delete</th>
+
+    </tr>
+
+        <tr>
+            <td>Milk</td>
+            <td>1</td>
+            <td> edit </td>
+            <td> delete </td>
+        </tr>
+    <tr>
+            <td>Creme Fraiche</td>
+            <td>2</td>
+        <td> <button> </button></td>
+        <td> delete </td>
+    </tr>
+
+</table>
 
 
 </html>
