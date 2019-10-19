0e45a6b794a90c0ba1a32b372800eb83387a5149
Ullapool <thomaslang82@bluewin.ch>
2019-10-19 14:06:26 +0200

    tidied code fixed back button issue special thanks to Nick alias Bob

--- smartFridge/add_remove.php
+++ smartFridge/add_remove.php
@@ -3,6 +3,64 @@
 
 <head>
     <title>"SmartFridge"</title>
+    <link rel="stylesheet" href="add_remove.css" type="text/css" media="screen">
+
+    <script type="text/javascript">
+        var amount = 1;
+
+        function increaseValue() {
+            document.getElementById("changeValue").stepUp();
+            amount++;
+
+        }
+
+        function decreaseValue() {
+            if(amount > 0) {
+                amount = document.getElementById("changeValue").stepDown();
+                amount--;
+            }
+            else {
+                document.write("Amount can't be smaller than 0!");
+                amount = 0;
+            }
+        }
+    </script>
+
+</head>
+<body>
+<a href="overview.php" class="previous round">&#8249;</a>
+<h1>Increase or reduce/remove this product</h1>
+<div class="image-counter">
+    <img src="http://lorempixel.com/400/200/" alt="placeholder">
+    <h3>Amount:</h3>
+    <input type="number" id="changeValue" onchange="name=this.amount">
+    <div class="buttons">
+        <div class="round-button-outer">
+            <div class="round-button green">
+                <a type="submit" onclick="increaseValue()" class="round-button">Increase</a>
+            </div>
+        </div>
+        <div class="round-button-outer">
+            <div class="round-button red">
+                <a type="submit" onclick="decreaseValue()" class="round-button">Reduce</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+<!--<!DOCTYPE html>
+<html>
+
+<head>
+    <title>"SmartFridge"</title>
     <link rel="stylesheet" href="add_remove.css" type="text/css" media="screen" >
 
      <script type="text/javascript">
@@ -30,7 +88,9 @@
 
 
 <body>
+
 <a href="overview.php" class="previous round">&#8249;</a>
+
 <h1>  Increase or reduce/remove this product</h1>
 
 <div class="image-counter">
